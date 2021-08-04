da6c90287969f17ac1fdee3ddc0f8671e0d51558
Nrayhan89 <nrayhan90@gmail.com>
2021-08-04 15:06:40 +0600

    changed sum fun

--- sum_arr.c
+++ sum_arr.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 int sum(int arr[], int n) {
 int s = 0;
 int i;
@@ -5,4 +6,16 @@ for (i=0;i<n;i++){
 s += arr[i];
 }
 return s;
+}
+int main() {
+int arr[100];
+int i,j,k,n;
+printf("Enter size of array ");
+// take array size input
+for(i=0;i<n;i++)
+{
+// take array elements input
+}
+printf("sum of the elements of this array is %d", sum(arr, n));
+return 0;
 }
